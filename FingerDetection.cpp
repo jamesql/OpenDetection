e3975883f3d7b36c9f50593b482745cfcbaadd44
James A <contactsmitteh@gmail.com>
2019-11-24 10:26:55 -0500

    Update FingerDetection.cpp

--- FingerDetection.cpp
+++ FingerDetection.cpp
@@ -1,12 +1,12 @@
 #include <FingerDetection.h>
 
 // Proc a single image
-void FingerDetection(IplImage* procImage) {
+void FingerDetectiong::FingerDetection(IplImage* procImage) {
 
 
 }
 
 // Create own VideoFeed obj
-void FingerDetection(int n) {
+void FingerDetectiong::FingerDetection(int n) {
 
 }

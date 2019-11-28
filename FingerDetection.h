4a3d7a3db06ca1d81b46aa451fc0b0ffcee5bb28
James A <contactsmitteh@gmail.com>
2019-11-27 21:36:53 -0500

    Addition onto fingerdetection.h

--- FingerDetection.h
+++ FingerDetection.h
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "cv.h"
+#include "cvcore.h"
+
+#define OD_FD_METHOD_REG 1
+#define OD_FD_METHOD_CUSTOM 2
 
 class FingerDetection {
   public:
     FingerDetection(void);
+    int returhCount();
+    int countFromImage(IplImage* img);
+    IplImage* getMaskedImageFeed();
+    IplImage* getMaskedImageFrame(IplImage* img);
+    
   
   private:
+  int c;
+  int count;
+  double threshold;
+  IplImage* mask;
+  IplImage* frame;
+  CvCapture* feed;
   
 };
